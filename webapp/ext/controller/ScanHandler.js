49fb31199bdb8cdffb82733498c9b067edd1032b
Zsofia Magyar <zsofia.magyar@nttdata.com>
2026-08-06 14:17:31 +0000

    260806

--- webapp/ext/controller/ScanHandler.js
+++ webapp/ext/controller/ScanHandler.js
@@ -3,10 +3,135 @@ sap.ui.define([
     "sap/m/Input",
     "sap/m/Button",
     "sap/m/MessageToast",
-    "sap/m/MessageBox"
-], function (Dialog, Input, Button, MessageToast, MessageBox) {
+    "sap/m/MessageBox",
+    "sap/m/SelectDialog",
+    "sap/m/StandardListItem",
+    "sap/ui/model/Filter"
+], function (Dialog, Input, Button, MessageToast, MessageBox, SelectDialog, StandardListItem, Filter) {
     "use strict";
-//klnlkhnk
+
+    // ====== Gép feloldása - üres lista esetén megkérdez, egyébként a meglévő sorból olvas vissza ======
+    function resolveEquipment(oSessionContext, oModel) {
+        var oOperationsBinding = oModel.bindList("_Operations", oSessionContext);
+
+        return oOperationsBinding.requestContexts(0, 1).then(function (aOpContexts) {
+            if (aOpContexts.length > 0) {
+                // van már tétel a listán - az onnan érvényes gépet vesszük át, nem kérdezünk újra
+                return aOpContexts[0].requestProperty("EquipmentId");
+            }
+
+            // üres a lista - munkahely alapján fel kell oldani a gépet
+            return oSessionContext.requestProperty("WorkCenterId").then(function () {
+                var sWorkCenterId = oSessionContext.getProperty("WorkCenterId");
+
+                var oEquipmentBinding = oModel.bindList("/Equipment", null, [], [
+                    new Filter("CrhdArbpl", "EQ", sWorkCenterId)
+                ]);
+
+                return oEquipmentBinding.requestContexts().then(function (aEquContexts) {
+                    if (aEquContexts.length === 0) {
+                        // Egyelőre nem minden munkahelyhez van gép rendelve - engedjük tovább.
+                        // Ha ez a jövőben kötelezővé válik, itt lehet majd hibát dobni, pl.:
+                        // MessageBox.error("Nincs gép rendelve ehhez a munkahelyhez!", { title: "Hiba" });
+                        // return null; // ez blokkolná a scan-t
+                        return undefined;
+                    }
+
+                    if (aEquContexts.length === 1) {
+                        return aEquContexts[0].getObject().EquiEqunr;
+                    }
+
+                    // több gép - választódialógus, a scan csak választás után indulhat
+                    return new Promise(function (resolve) {
+                        var oDialog = new SelectDialog({
+                            title: "Válasszon gépet",
+                            items: aEquContexts.map(function (oCtx) {
+                                var oEq = oCtx.getObject();
+                                return new StandardListItem({
+                                    title: oEq.EquiEqunr,
+                                    description: oEq.EqktEqktx
+                                });
+                            }),
+                            confirm: function (oEvent) {
+                                var sSelected = oEvent.getParameter("selectedItem").getTitle();
+                                oDialog.destroy();
+                                resolve(sSelected);
+                            },
+                            cancel: function () {
+                                oDialog.destroy();
+                                resolve(null);
+                            }
+                        });
+                        oDialog.open();
+                    });
+                });
+            });
+        }).catch(function (oError) {
+            console.error("[Equipment] resolve error:", oError);
+            MessageBox.error("Nem sikerült meghatározni a gépet.");
+            return null;
+        });
+    }
+
+    // ====== A barcode-beviteli dialógus - most már gép-paraméterrel ======
+    function openBarcodeDialog(oModel, oSessionContext, sEquipmentId) {
+        var oInput = new Input({
+            placeholder: "Vonalkód beolvasása",
+            submit: function () {
+                oDialog.close();
+                handleBarcode(oInput.getValue());
+            }
+        });
+
+        var oDialog = new Dialog({
+            title: "Beolvasás",
+            content: [oInput],
+            beginButton: new Button({
+                text: "OK",
+                press: function () {
+                    oDialog.close();
+                    handleBarcode(oInput.getValue());
+                }
+            }),
+            endButton: new Button({
+                text: "Mégse",
+                press: function () { oDialog.close(); }
+            }),
+            afterClose: function () { oDialog.destroy(); }
+        });
+
+        oDialog.open();
+
+        function handleBarcode(sBarcode) {
+            if (!sBarcode) { return; }
+
+            var oListBinding = oModel.bindList("_Operations", oSessionContext);
+            var oHeaderContext = oListBinding.getHeaderContext();
+
+            var oAction = oModel.bindContext(
+                "com.sap.gateway.srvd.zui_mes_pp_massconf.v0001.createByBarcode(...)",
+                oHeaderContext
+            );
+            oAction.setParameter("barcode_value", sBarcode);
+            oAction.setParameter("equipment_id", sEquipmentId);
+            oAction.execute().then(function () {
+                return oAction.getBoundContext().requestObject();
+            }).then(function (oResult) {
+                var iCount = oResult && oResult.value ? oResult.value.length : 0;
+
+                if (iCount === 0) {
+                    MessageBox.error("Nincs ilyen vonalkód, vagy nem sikerült felvinni.");
+                    return;
+                }
+
+                MessageToast.show(iCount + " tétel hozzáadva.");
+                return oSessionContext.requestSideEffects([{ $NavigationPropertyPath: "_Operations" }]);
+            }).catch(function (oError) {
+                MessageBox.error("Hiba a felvitel közben: " + oError.message);
+            });
+        }
+    }
+
     return {
         onScanBarcode: function (aContexts) {
             var oSessionContext = Array.isArray(aContexts) ? aContexts[0] : aContexts;
@@ -18,60 +143,14 @@ sap.ui.define([
 
             var oModel = oSessionContext.getModel();
 
-            var oInput = new Input({
-                placeholder: "Vonalkód beolvasása",
-                submit: function () {
-                    oDialog.close();
-                    handleBarcode(oInput.getValue());
+            resolveEquipment(oSessionContext, oModel).then(function (sEquipmentId) {
+                if (sEquipmentId === null) {
+                    // user Mégse-t nyomott a gép-választó dialóguson - ne folytatódjon a scan
+                    return;
                 }
+                // sEquipmentId lehet undefined (nincs gép rendelve) - egyelőre engedjük tovább
+                openBarcodeDialog(oModel, oSessionContext, sEquipmentId);
             });
-
-            var oDialog = new Dialog({
-                title: "Beolvasás",
-                content: [oInput],
-                beginButton: new Button({
-                    text: "OK",
-                    press: function () {
-                        oDialog.close();
-                        handleBarcode(oInput.getValue());
-                    }
-                }),
-                endButton: new Button({
-                    text: "Mégse",
-                    press: function () { oDialog.close(); }
-                }),
-                afterClose: function () { oDialog.destroy(); }
-            });
-
-            oDialog.open();
-
-            function handleBarcode(sBarcode) {
-                if (!sBarcode) { return; }
-
-                var oListBinding = oModel.bindList("_Operations", oSessionContext);
-                var oHeaderContext = oListBinding.getHeaderContext();
-
-                var oAction = oModel.bindContext(
-                    "com.sap.gateway.srvd.zui_mes_pp_massconf.v0001.createByBarcode(...)",
-                    oHeaderContext
-                );
-                oAction.setParameter("barcode_value", sBarcode);
-                oAction.execute().then(function () {
-                    return oAction.getBoundContext().requestObject();
-                }).then(function (oResult) {
-                    var iCount = oResult && oResult.value ? oResult.value.length : 0;
-
-                    if (iCount === 0) {
-                        MessageBox.error("Nincs ilyen vonalkód, vagy nem sikerült felvinni.");
-                        return;
-                    }
-
-                    MessageToast.show(iCount + " tétel hozzáadva.");
-                    return oSessionContext.requestSideEffects([{ $NavigationPropertyPath: "_Operations" }]);
-                }).catch(function (oError) {
-                    MessageBox.error("Hiba a felvitel közben: " + oError.message);
-                });
-            }
         },
 
         onDeleteRow: function (oTableContext, aSelectedContexts) {
--- webapp/ext/controller/SessionObjectPageExt.controller.js
+++ webapp/ext/controller/SessionObjectPageExt.controller.js
@@ -38,10 +38,17 @@ sap.ui.define([
             }
 
             var aFlagNames = ["IsSterilization", "IsAnodizing", "IsLaserMarking",
-                              "IsManualOp", "IsMeo", "IsLabeling", "IsWaterjet"];
+                "IsManualOp", "IsMeo", "IsLabeling", "IsWaterjet"];
 
-            Promise.all([oContext.requestProperty(aFlagNames), oTable.initialized()]).then(function () {
-                console.log("[ButtonVisibility] flags loaded AND table initialized");
+            Promise.all([
+                oContext.requestProperty(aFlagNames),
+                oTable.initialized(),
+                this._getWaterjetStarted(oContext)
+            ]).then(function (aResults) {
+                console.log("[ButtonVisibility] flags loaded, table initialized, waterjet state resolved");
+
+                var bWaterjetStarted = aResults[2];
+                var bIsWaterjet = oContext.getProperty("IsWaterjet") === "X";
 
                 var mVisibility = {
                     "confirmSterilization": oContext.getProperty("IsSterilization") === "X",
@@ -50,8 +57,8 @@ sap.ui.define([
                     "confirmManualOp": oContext.getProperty("IsManualOp") === "X",
                     "confirmMeo": oContext.getProperty("IsMeo") === "X",
                     "confirmLabeling": oContext.getProperty("IsLabeling") === "X",
-                    "startWaterjet": oContext.getProperty("IsWaterjet") === "X",
-                    "confirmWaterjet": oContext.getProperty("IsWaterjet") === "X"
+                    "startWaterjet": bIsWaterjet && !bWaterjetStarted,
+                    "confirmWaterjet": bIsWaterjet && bWaterjetStarted
                 };
 
                 console.log("[ButtonVisibility] computed visibility:", JSON.stringify(mVisibility));
@@ -73,8 +80,59 @@ sap.ui.define([
                         oButton.setVisible(mVisibility[sActionName]);
                     });
                 });
-            }).catch(function (oError) {
+
+                // Vízvágás köztes állapot: törlés/scan gombok tiltása
+                var bLockListActions = bIsWaterjet && bWaterjetStarted;
+                console.log("[ListLock] bLockListActions:", bLockListActions);
+
+                this._setListActionsEnabled(oTable, oView, !bLockListActions);
+
+            }.bind(this)).catch(function (oError) {
                 console.log("[ButtonVisibility] failed:", oError);
+            });
+        },
+
+        // A lista első sorából olvassuk vissza a WaterjetStarted állapotot -
+        // mivel a lista mindig homogén állapotú, elég egy sort megnézni
+        _getWaterjetStarted: function (oSessionContext) {
+            var oModel = this.base.getModel();
+            var oOperationsBinding = oModel.bindList("_Operations", oSessionContext);
+
+            return oOperationsBinding.requestContexts(0, 1).then(function (aOpContexts) {
+                if (aOpContexts.length === 0) {
+                    return false;
+                }
+                return aOpContexts[0].requestProperty("WjStarted").then(function () {
+                    return aOpContexts[0].getProperty("WjStarted") === "X";
+                });
+            }).catch(function (oError) {
+                console.error("[Waterjet] state check error:", oError);
+                return false;
+            });
+        },
+
+        // Törlés és beolvasás gombok engedélyezése/tiltása
+        _setListActionsEnabled: function (oTable, oView, bEnabled) {
+            // Törlés: "deleteRow" custom action
+            var aDeleteButtons = oTable.findAggregatedObjects(true, function (oControl) {
+                return oControl.getId && oControl.getId().indexOf("deleteRow") > -1
+                    && typeof oControl.setEnabled === "function";
+            });
+            console.log("[ListLock] delete buttons found:", aDeleteButtons.length);
+            aDeleteButtons.forEach(function (oButton) {
+                console.log("[ListLock] setting delete button", oButton.getId(), "enabled =", bEnabled);
+                oButton.setEnabled(bEnabled);
+            });
+
+            // Beolvasás: "scanBarcode" custom action
+            var aScanButtons = oTable.findAggregatedObjects(true, function (oControl) {
+                return oControl.getId && oControl.getId().indexOf("scanBarcode") > -1
+                    && typeof oControl.setEnabled === "function";
+            });
+            console.log("[ListLock] scan buttons found:", aScanButtons.length);
+            aScanButtons.forEach(function (oButton) {
+                console.log("[ListLock] setting scan button", oButton.getId(), "enabled =", bEnabled);
+                oButton.setEnabled(bEnabled);
             });
         }
     });
